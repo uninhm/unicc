c7fb1c237b9462b01369da65cc1dd5fa8ae840ef
Unai Hernandez Minaberry <unihernandez22@gmail.com>
2025-02-07 12:20:12 +0100

    Stage 2 - Code generation

--- src/codegen.rs
+++ src/codegen.rs
@@ -22,10 +22,33 @@ fn generate_stmt(stmt: Statement) -> String {
     let mut res = String::new();
     match stmt {
         Statement::Return(expr) => {
-            let Expression::Int(x) = expr;
-            res.push_str(&format!("mov ${}, %rax\nret\n", x));
+            res.push_str(&generate_expr(expr));
+            res.push_str("ret\n");
         }
         _ => panic!("Statement {:?} code generation not implemented", stmt)
+    }
+    res
+}
+
+fn generate_expr(expr: Expression) -> String {
+    let mut res = String::new();
+    match expr {
+        Expression::Int(x) => {
+            res.push_str(&format!("mov ${x}, %rax\n"));
+        }
+        Expression::UnaryOperation(op, expr) => {
+            res.push_str(&generate_expr(*expr));
+            match op {
+                UnaryOperator::Negation => res.push_str("neg %rax\n"),
+                UnaryOperator::BitwiseNot => res.push_str("not %rax\n"),
+                UnaryOperator::LogicNot => {
+                    res.push_str("cmp $0, %rax\n");
+                    res.push_str("mov $0, %rax\n");
+                    res.push_str("sete %al\n");
+                }
+                _ => panic!("Unary operator {:?} code generation not implemented", op)
+            }
+        }
     }
     res
 }
