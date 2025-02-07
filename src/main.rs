91b4f017fd639e8e28224485db2ecde3a5aa90d3
Unai Hernandez Minaberry <unihernandez22@gmail.com>
2025-02-07 11:42:16 +0100

    Stage 1

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,25 @@ mod lexer;
 mod parser;
 mod codegen;
 
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::Path;
+
 fn main() {
-    let tokens = lexer::lex("int main () { return 0; }");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("Usage: unicc <filename>");
+        return;
+    }
+
+    let filepath = Path::new(&args[1]);
+    let contents = fs::read_to_string(filepath).expect("File not found");
+
+    let tokens = lexer::lex(&contents);
     let ast = parser::parse(tokens);
     let code = codegen::generate(ast);
+
     println!("{}", code);
 }
